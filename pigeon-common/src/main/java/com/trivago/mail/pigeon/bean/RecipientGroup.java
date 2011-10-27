f3f118f3e5bc74372b0b4cadef3186fa8ae773a7
Christian Krause <christian.krause@trivago.com>
2011-10-27 10:13:29 +0200

    user_id => group_id removed email

--- pigeon-common/src/main/java/com/trivago/mail/pigeon/bean/RecipientGroup.java
+++ pigeon-common/src/main/java/com/trivago/mail/pigeon/bean/RecipientGroup.java
@@ -15,11 +15,9 @@ public class RecipientGroup
 {
 	private Node dataNode;
 
-	public static final String ID = "user_id";
+	public static final String ID = "group_id";
 
 	public static final String NAME = "name";
-
-	public static final String EMAIL = "email";
 
 	public static final String DATE = "date";
 
