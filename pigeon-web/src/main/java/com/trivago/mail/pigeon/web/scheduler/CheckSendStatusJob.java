a11910b90108fc4b3110ea3aa0ffa2ecf73236d8
Mario Mueller <mario.mueller@xenji.com>
2011-11-08 17:47:29 +0100

    added quarz scheduler

---
+++ pigeon-web/src/main/java/com/trivago/mail/pigeon/web/scheduler/CheckSendStatusJob.java
@@ -0,0 +1,20 @@
+package com.trivago.mail.pigeon.web.scheduler;
+
+import com.vaadin.ui.ProgressIndicator;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class CheckSendStatusJob implements Job
+{
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException
+	{
+		ProgressIndicator pi = (ProgressIndicator) context.get("progressIndicator");
+		long newsletterId = (Long) context.get("newsletterId");
+		long recipientGroupId = (Long) context.get("recipientGroupId");
+		float actualValue = (Float)pi.getValue();
+
+		
+	}
+}
