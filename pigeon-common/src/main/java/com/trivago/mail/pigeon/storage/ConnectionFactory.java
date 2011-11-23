6a378a8c627bfe7f4cbdf8e075165c4a9d9e3b59
Mario Mueller <mario.mueller@xenji.com>
2011-11-23 18:27:49 +0100

    Smaller fixes

--- pigeon-common/src/main/java/com/trivago/mail/pigeon/storage/ConnectionFactory.java
+++ pigeon-common/src/main/java/com/trivago/mail/pigeon/storage/ConnectionFactory.java
@@ -8,9 +8,10 @@ import org.neo4j.kernel.AbstractGraphDatabase;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.server.WrappingNeoServerBootstrapper;
 
-public class ConnectionFactory {
+public class ConnectionFactory
+{
 
-	private static AbstractGraphDatabase graphDb;
+	private static AbstractGraphDatabase graphDb = new EmbeddedGraphDatabase(Settings.create().getConfiguration().getString("neo4j.path"));
 
 	private static Index<Node> newsletterIndex;
 
@@ -36,12 +37,27 @@ public class ConnectionFactory {
 	 * The wrapper for the graph db to be exposed as server
 	 */
 	private static WrappingNeoServerBootstrapper srv;
-	
-	static {
-		graphDb = new EmbeddedGraphDatabase(Settings.create().getConfiguration().getString("neo4j.path"));
+
+	static
+	{
+		newsletterIndex = graphDb.index().forNodes("newsletter");
+		userIndex = graphDb.index().forNodes("user");
+		groupIndex = graphDb.index().forNodes("group");
+		senderIndex = graphDb.index().forNodes("sender");
+		senderIndex = graphDb.index().forNodes("bounce");
+		campaignIndex = graphDb.index().forNodes("campaign");
+		templateIndex = graphDb.index().forNodes("template");
+		if (notStarted)
+		{
+			srv = new WrappingNeoServerBootstrapper(graphDb);
+			srv.start();
+			registerShutdownHook();
+			notStarted = false;
+		}
 	}
 
-	private ConnectionFactory() {
+	private ConnectionFactory()
+	{
 	}
 
 	/**
@@ -49,95 +65,67 @@ public class ConnectionFactory {
 	 *
 	 * @return the graphdb instance
 	 */
-	public static GraphDatabaseService getDatabase() {
-		synchronized (graphDb) {
-			if (notStarted) {
-				srv = new WrappingNeoServerBootstrapper(graphDb);
-				srv.start();
-
-				newsletterIndex = graphDb.index().forNodes("newsletter");
-				userIndex = graphDb.index().forNodes("user");
-				groupIndex = graphDb.index().forNodes("group");
-				senderIndex = graphDb.index().forNodes("sender");
-				senderIndex = graphDb.index().forNodes("bounce");
-				campaignIndex = graphDb.index().forNodes("campaign");
-				templateIndex = graphDb.index().forNodes("template");
-
-				registerShutdownHook();
-				notStarted = false;
-			}
-		}
+	public static GraphDatabaseService getDatabase()
+	{
 		return graphDb;
 	}
 
-	public static Index<Node> getNewsletterIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getNewsletterIndex()
+	{
 		return newsletterIndex;
 	}
 
-	public static Index<Node> getUserIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getUserIndex()
+	{
 		return userIndex;
 	}
 
-	public static Index<Node> getGroupIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getGroupIndex()
+	{
 		return groupIndex;
 	}
 
-	public static Index<Node> getSenderIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getSenderIndex()
+	{
 		return senderIndex;
 	}
 
-	public static Index<Node> getBounceIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getBounceIndex()
+	{
 		return bounceIndex;
 	}
 
-	public static Index<Node> getCampaignIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getCampaignIndex()
+	{
 		return campaignIndex;
 	}
 
-	public static Index<Node> getTemplateIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getTemplateIndex()
+	{
 		return templateIndex;
 	}
 
-	public static Index<Node> getTagIndex() {
-		if (graphDb == null) {
-			getDatabase();
-		}
+	public static Index<Node> getTagIndex()
+	{
 		return tagIndex;
 	}
 
-	private static void shutdown() {
+	private static void shutdown()
+	{
 		srv.stop();
 		graphDb.shutdown();
 	}
 
-	private static void registerShutdownHook() {
+	private static void registerShutdownHook()
+	{
 		// Registers a shutdown hook for the Neo4j and index service instances
 		// so that it shuts down nicely when the VM exits (even if you
 		// "Ctrl-C" the running example before it's completed)
-		Runtime.getRuntime().addShutdownHook(new Thread() {
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
 			@Override
-			public void run() {
+			public void run()
+			{
 				shutdown();
 			}
 		});
